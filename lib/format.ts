59262d82ea572006f4bbeefd983397cf52c93210
delsonlife <44551767+delsonlife@users.noreply.github.com>
2026-06-15 19:36:37 +0100

    Create format.ts

---
+++ lib/format.ts
@@ -0,0 +1,17 @@
+export function formatDate(dateString: string): string {
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(new Date(dateString));
+}
+
+export function formatDateTime(dateString: string): string {
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(dateString));
+}
